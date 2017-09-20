0c4cd1a66a73e334a492b40af87109daf6491b8b
apri <m.apriluzik@gmail.com>
2017-09-20 15:25:05 +0900

    로그인/SignUpActivity

--- app/src/main/java/com/apriluziknaver/numbaseball/MainActivity.java
+++ app/src/main/java/com/apriluziknaver/numbaseball/MainActivity.java
@@ -15,15 +15,17 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 //SignUp 가서 로그인하고 다시 돌아오면 SignUp버튼 사라져있고 내정보보임.
 
+    TextView tv;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.tv);
 
-
-
+        tv.setText(name);
     }
 
     public void StartBtn(View view){
@@ -37,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
         Intent intent=new Intent(this,SignUpActivity.class);
         startActivityForResult(intent,1000);
     }
-
+    String name;
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-
+        name= data.getStringExtra("Name");
 
     }
 }
--- app/src/main/java/com/apriluziknaver/numbaseball/SignUpActivity.java
+++ app/src/main/java/com/apriluziknaver/numbaseball/SignUpActivity.java
@@ -1,10 +1,20 @@
 package com.apriluziknaver.numbaseball;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+    ImageView loginBtn;
+    EditText editname;
+    String name;
+
 
     Intent intent;
 
@@ -16,5 +26,24 @@ public class SignUpActivity extends AppCompatActivity {
         intent=getIntent();
 
 
+
+//        loginBtn.setOnClickListener(listener);
+
+
+
     }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            name = editname.getText().toString();
+
+            Toast.makeText(SignUpActivity.this, name+"", Toast.LENGTH_SHORT).show();
+            setResult(RESULT_OK);
+            intent.putExtra("Name",name);
+            finish();
+        }
+
+    };
+
 }
